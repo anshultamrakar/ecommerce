74751f2c42f7f245d6a4bf5efbac29386030f6eb
Anshul Tamrakar <anshultamrakar487@gmail.com>
2023-06-10 12:10:49 +0530

    chnages

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { ToastContainer } from 'react-toastify';
 import { DataContext } from "../Context/DataContext";
 
-const categoryInput = ['Sofas' , "Bed" , "Tables" , "Chairs" , "Wardrobe" , "Dinning Table"]
+const categoryInput = ['Sofas' , "Bed" ,  "Tables" , "Chairs" , "Wardrobe" , "Dinning Table"]
+const ratingInput = ["4star" , "3star", "2star", "1star"]
 
 const Products = () => {
-  const {products , setProducts,  isLoading , handleAddToCart , handleWishList , checkboxFilter , setCheckBoxFilter , search , originalProductData , getProductData } = useContext(DataContext)
-  const [priceRange , setPriceRange] = useState("")
-  const [selectOption , setSelectOption] = useState("")
-  const [ratingOption , setRatingOption] = useState("")
+  const {products , setProducts , handleAddToCart , handleWishList , checkboxFilter , setCheckBoxFilter , search , originalProductData , getProductData  } = useContext(DataContext)
+  const [priceRange , setPriceRange] = useState("2600")
+  const [filteredRating , setFilterRating] = useState(products)
+  const [sortOption , setSortOption] = useState("option1")
+  const [ratingOption , setRatingOption] = useState("4star")
+
 
 useEffect(() => {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
@@ -21,42 +23,35 @@ useEffect(() => {
 },[])
 
 
-useEffect(() => {
-    const filterSearchResult = originalProductData.filter(product => product.title.toLowerCase().includes(search.toLowerCase()))
-    setProducts(filterSearchResult)
-}, [originalProductData,  search])
+const handleClear = () => {
+ console.log("hello")
+}
 
 
+const onInputChange = (e) => {
+ setPriceRange(e.target.value)
+ const resultFilter = originalProductData.filter(item => item.price === priceRange)
+ setProducts(resultFilter)
+}
 
-useEffect(() => {
-  if(selectOption === "option1"){
-    const filterResult = [...products].sort((a, b) => a.price - b.price)
-    setProducts(filterResult)
-  }else{
-    const filterResult =  [...products].sort((a, b) => b.price - a.price)
-    setProducts(filterResult)
-  }
-},[selectOption])
+const handleSorting = (e) => {
+ setSortOption(e.target.value)
+ if(sortOption === "option1"){
+  setProducts([...products].sort((a,b) => b.price -a.price))
+ }
+ if(sortOption === "option2"){
+  setProducts([...products].sort((a,b) => a.price -b.price))
+ }
+}
 
+const handleRatingInput = (e) => {
+ setRatingOption(e.target.value)
+ const filterResult = originalProductData.filter(item => item.rating === ratingOption)
+ setProducts(filterResult)
+}
 
-
-useEffect(() => {
-  const filterResult = originalProductData.filter(item => item.rating === ratingOption)
-  setProducts(filterResult)
-},[ratingOption])
-
-
-// useEffect(() => {
-//   const filterResult = originalProductData.filter(item => item.price === priceRange)
-//   setProducts(filterResult)
-// },[priceRange])
-
-
-console.log({products})
-
-
-  const handleCheckboxInput = (e) => {
-   const {checked , value} = e.target
+ const handleCheckInput = (e) => {
+  const {checked , value} = e.target
    if(checked){
     setCheckBoxFilter([...checkboxFilter , value])
    }else{
@@ -65,26 +60,25 @@ console.log({products})
    }
   }
 
-
-  const filteredProducts =  checkboxFilter.length > 0 ?  products.filter(item => checkboxFilter.includes(item.categoryName)) : products
-  
+ 
+ const filteredProducts =  checkboxFilter.length > 0 ?  products.filter(item => checkboxFilter.includes(item.categoryName)) : products
 
   return (
     <div className="Product_list">
       <aside>
         <div className="filter_heading">
           <h2>Filters</h2>
-          <h4 style = {{textDecoration : "underline"}}>Clear</h4>
+          <h4 onClick = {handleClear} style = {{textDecoration : "underline"}}>Clear</h4>
         </div>
         <div className="filter_price">
-          <label htmlFor="pricerange">Price</label>
-          <input id = "pricerange" type="range" min= "2999" max= "55000"  />
+          <label htmlFor="pricerange">Price </label>
+          <input id = "pricerange" type="range" defaultValue={2600} min = "2600" max = "55000"value = {priceRange} onChange={onInputChange}/>
         </div>
         <div className="filter_category">
           <h4>Category</h4>
           {categoryInput.map(item => (
               <div className="filter_inputs">
-              <input type="checkbox" id= {item}  value = {item} onChange={handleCheckboxInput} />
+              <input type="checkbox" id= {item}  value = {item}  onChange={handleCheckInput}  />
               <label htmlFor= {item}>{item}</label>
               </div>
             ))
@@ -92,61 +86,46 @@ console.log({products})
         </div>
         <div className="filter_ratings">
           <h4>Rating</h4>
-          <div className="filter_inputs">
-          <input type="radio" id="ratings" value = {"4star"}  checked = {ratingOption === "4star"} onChange={(e) => setRatingOption(e.target.value)} />
-          <label htmlFor="ratings">4 Star & above </label>
-          </div>
-          <div className="filter_inputs">
-          <input type="radio" id="ratings" value = {"3star"}  checked = {ratingOption === "3star"} onChange={(e) => setRatingOption(e.target.value)} />
-          <label htmlFor="ratings">3 Star & above</label>
-          </div>
-         <div className="filter_inputs">
-         <input
-            type="radio"
-            id="ratings"
-            value = {"2star"}  checked = {ratingOption === "2star"} onChange={(e) => setRatingOption(e.target.value)}
-          />
-          <label htmlFor="ratings">2 Star & above</label>
-         </div>
-          <div className="filter_inputs">
-          <input type="radio" id="ratings"  value = {"1star"}  checked = {ratingOption === "1star"} onChange={(e) => setRatingOption(e.target.value)}/>
-          <label htmlFor="ratings">1 Start & above</label>
-          </div>
+          {ratingInput.map(item => (
+            <div className="filter_inputs">
+            <input type="radio" id= {item}  value = {item}  checked = {ratingOption === item} onChange={handleRatingInput} />
+            <label htmlFor= {item}>{item} & above </label>
+            </div>
+          ))}
         </div>
         <div className="filter_sort">
           <h4>Sort By</h4>
-          <div className="filter_inputs">
-          <input type="radio" id="lowHigh" value = {"option1"} checked = {selectOption === "option1"} onChange={(e) => setSelectOption(e.target.value)}/>
-          <label htmlFor="lowHigh">Price - Low to High</label>
-          </div>
-          <div className="filter_inputs">
-          <input type="radio" id="HighLow" value = {"option2"}  checked = {selectOption === "option2"}   onChange={(e) => setSelectOption(e.target.value)} />
-          <label htmlFor="HighLow">Price - High to low</label>
-          </div>
+            <div className="filter_inputs">
+              <input type="radio" id= "sortprice" value = {"option1"} checked = {sortOption === "option1"}  onChange={handleSorting}/>
+              <label htmlFor= "sortprice"> Price - Low to High</label>
+            </div>
+            <div className="filter_inputs">
+              <input type="radio" id= "sortprice" value={"option2"}  checked = {sortOption === "option2"}  onChange={handleSorting}/>
+              <label htmlFor= "sortprice">Price - High To Low </label>
+            </div>
         </div>
       </aside>
       <div className="product_listItems">
         <h2>
-          Showing All Products{" "}
+          Showing All Products
           <span className="qty_product">({`Showing ${products.length} products`})</span>
         </h2>
         <ul className="product-items">
-          {isLoading ? <p>Loading.......</p> : filteredProducts.map((product) => (
-            <li className="product-list" key={product._id}>
+          {filteredProducts.map((product) => (
+            <li className="product-list" key={product.id}>
              <Link to = {`${product._id}`}><img src={product.img} /></Link> 
               <h4>{product.title}</h4>
               <p> ₹ {product.price}</p>
               <button disabled = {product.isAddedToCart ? true :false} onClick={() => handleAddToCart(product.id)}>
                 {!product.isAddedToCart ? "Add to cart"  : <Link style = {{textDecoration : "none" , color : "#fff"}} to = "/cart">Go to cart</Link> }
               </button>
-              <div  className="like-wishlist">
+              <div className="like-wishlist">
                 <div onClick={() => handleWishList(product.id)} className="wishlist-button">
-                { !product.isAddedToWish ? <AiOutlineHeart /> : <AiFillHeart/> }
+                {!product.isAddedToWish ? <AiOutlineHeart /> : <AiFillHeart/> }
               </div>
               </div>
             </li>
-           ))
-           }
+           ))}
         </ul>
       </div>
     </div>
